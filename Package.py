f17c20f56b63e53bcc642fafbed53991e1870168
Stobio19 <128319411+Stobio19@users.noreply.github.com>
2023-04-07 23:08:18 -0500

    Update Package.py
    Adding comments.

--- Package.py
+++ Package.py
@@ -1,80 +1,83 @@
-from abc import abstractmethod
+from abc import abstractmethod # Importing the abstract method
 
 
-class Package(object):
-    @abstractmethod
-    def __init__(self, id: int = 0, weight: float = 0.0,
-                 description: str = " "):
-        if type(id) != int:
+class Package(object): # Define principal class (Package)
+    @abstractmethod # Use abstract method 
+    def __init__(self, id: int = 0, weight: float = 0.0, 
+                 description: str = " "): # Define constructor and initializing variables
+        # Define conditionals for variables
+        if type(id) != int: # Validating that the incoming data is int 
             print("Please enter a valid id.")
             return
 
-        if weight < 0 and type(weight) != float:
+        if weight < 0 and type(weight) != float: # Validating that the incoming data is float and greater than 0
             print("Please enter a valid weight.")
             return
 
-        if type(description) != str:
+        if type(description) != str: # Validating that the incoming data is str
             print("Please enter a valid description.")
             return
-
+        
+        # Define variables
         self._id: int = id
         self._weight: float = weight
         self._description: str = description
         self.W_GR_100: float = 2
         self._cost: float = 0
-
+    
+    # Using decorators with property
     @property
-    def id(self) -> int:
+    def id(self) -> int: # Get id
         return self._id
 
-    @id.setter
+    @id.setter # Setter id
     def id(self, id_new: int) -> None:
-        if id_new > 0 and isinstance(id_new, int):
+        if id_new > 0 and isinstance(id_new, int): # Validating
             self._id = id_new
         else:
             print("Please enter a valid id")
 
     @property
-    def weight(self) -> float:
+    def weight(self) -> float: # Get weight
         return self._weight
 
-    @weight.setter
+    @weight.setter # Setter weight
     def weight(self, weight_new: float) -> None:
-        if weight_new > 0 and isinstance(weight_new, float):
+        if weight_new > 0 and isinstance(weight_new, float): # Validating
             self._weight = weight_new
         else:
             print("Please enter a valid weight.")
 
     @property
-    def description(self) -> str:
+    def description(self) -> str: # Get description
         return self._description
 
-    @description.setter
-    def description(self, description_new: str) -> None:
+    @description.setter # Setter description
+    def description(self, description_new: str) -> None: # Validating
         if isinstance(description_new, str):
             self._description = description_new
         else:
             print("Please enter a valid description.")
 
     @property
-    def cost(self) -> float:
+    def cost(self) -> float: # Get cost
         return self._cost
 
-    @abstractmethod
-    def __str__(self) -> str:
+    @abstractmethod # Inheritable method
+    def __str__(self) -> str: # Method to print all the info
         return f"""\nId: {self._id}\nWeight: {self._weight}\ndescription: {self._description}\nShipping Cost: {self._cost}"""
 
-    @abstractmethod
+    @abstractmethod # Inheritable method
     def calculate(self) -> float:
         pass
 
-    def __eq__(self, other_package: 'Package'):
+    def __eq__(self, other_package: 'Package'): # Equal method, to compare two objects
         if isinstance(other_package, Package):
             return self._id == other_package._id and self._weight == other_package._weight and \
                 self._description == other_package._description
         return False
 
 
-test_1 = Package(35, 10, "descripcion paquete 32")
+test_1 = Package(35, 10, "descripcion paquete 32") # Prove test
 
-print(test_1)
+print(test_1) # Printing prove test
